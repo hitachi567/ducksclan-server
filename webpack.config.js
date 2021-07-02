c1c195dcbc96ed702d753380461cffb7dbc15798
hitachi567 <72521376+hitachi567@users.noreply.github.com>
2021-07-02 18:51:45 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpackNodeExternals = require('webpack-node-externals');
+process.env.NODE_ENV = 'production';
+
+module.exports = {
+    entry: './src/index.ts',
+    target: 'node',
+    mode: process.env.NODE_ENV,
+    externalsPresets: {
+        node: true
+    },
+    externals: [webpackNodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader'
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    }
+}
