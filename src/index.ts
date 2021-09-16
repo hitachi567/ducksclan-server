45866dfd2ec7a10d786400867213eb72b060f5db
hitachi567 <abidai790@gmail.com>
2021-09-16 20:54:53 +0300

    registration

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,21 @@ import Config from './Config';
 import Database from './database/Database';
 import { routs } from './routs';
 import Log from './utils/Log';
+import UserService from './services/user.service';
+import UserDatabase from './database/user.database';
 
 export const config = new Config();
 export const database = new Database();
 Database.createTables().then(main).catch(Log.error);
 
 function main() {
+    // Database.deleteTables()
+    // UserDatabase.getInstance().then(db => {
+    //     db.findAll().then(data => {
+    //         console.log(data.rows)
+    //     })
+    // })
+
     const app = initExpressApp(routs);
     const server = http.createServer(app);
     server.listen(config.port, listener);
