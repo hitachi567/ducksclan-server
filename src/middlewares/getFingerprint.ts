f4cbf37eb428d6ecf3127d45a434185797e5d710
hitachi567 <abidai790@gmail.com>
2021-09-25 11:48:12 +0300

    repair getting fingerprint

--- src/middlewares/getFingerprint.ts
+++ src/middlewares/getFingerprint.ts
@@ -1,9 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
+import Generator from '../lib/Generator';
 
 function getFingerprint(requset: Request, response: Response, next: NextFunction) {
-    response.locals.fingerprint = requset.fingerprint
-        ? requset.fingerprint.hash
-        : requset.ip;
+    if (requset.fingerprint) {
+        response.locals.fingerprint = requset.fingerprint.hash;
+    } else if (requset.cookies.id) {
+        response.locals.fingerprint = requset.cookies.id;
+    } else {
+        response.locals.fingerprint = Generator.generateSequense(32);
+    }
+    response.cookie('id', response.locals.fingerprint, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 });
     next();
 }
 
