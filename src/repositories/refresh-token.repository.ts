3312810b1d48a7818ff1f8f3937966c09de74568
hitachi567 <abidai790@gmail.com>
2022-01-09 11:40:58 +0300

    added methods to repositories

--- src/repositories/refresh-token.repository.ts
+++ src/repositories/refresh-token.repository.ts
@@ -1,4 +1,4 @@
-import { EntityManager } from 'typeorm';
+import { EntityManager, DeleteResult } from 'typeorm';
 import AbstractRepository from '../database/abstract.repository';
 import RefreshToken from '../entities/refresh-token';
 
@@ -7,6 +7,37 @@ export default class RefreshTokenRepository extends AbstractRepository<RefreshTo
     constructor(manager: EntityManager) {
 
         super(manager, RefreshToken);
+
+    }
+
+    removeNotRelevant(user_id?: string): Promise<DeleteResult> {
+
+        // let sql = `created_at + '30 day' <= current_timestamp`; // postgres syntax
+        let condition = `datetime(created_at, '+30 day') <= datetime('now')`; // sqlite syntax
+
+        let query = this.sql().delete().where(condition);
+
+        if (user_id) {
+            query.andWhere('token.user_id = :user_id', { user_id })
+        }
+
+        return query.execute();
+
+    }
+
+    removeByFingerprint(fingerprint: string): Promise<DeleteResult> {
+
+        let condition = 'token.fingerprint = :fingerprint';
+
+        let query = this.sql().delete().where(condition, { fingerprint });
+
+        return query.execute();
+
+    }
+
+    protected sql() {
+
+        return this.repository.createQueryBuilder('token');
 
     }
 
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,4 +1,4 @@
-import { EntityManager } from 'typeorm';
+import { EntityManager, DeleteResult } from 'typeorm';
 import AbstractRepository from '../database/abstract.repository';
 import User from '../entities/user';
 
@@ -7,6 +7,34 @@ export default class UserRepository extends AbstractRepository<User> {
     constructor(manager: EntityManager) {
 
         super(manager, User);
+
+    }
+
+    findOneByID(id: string): Promise<User | undefined> {
+
+        return this.repository.findOne({ where: { id } });
+
+    }
+
+    findOneByEmail(email: string): Promise<User | undefined> {
+
+        return this.repository.findOne({ where: { email } });
+
+    }
+
+    removeByID(id: string): Promise<DeleteResult> {
+
+        let condition = 'user.id = :id';
+
+        let query = this.sql().delete().where(condition, { id });
+
+        return query.execute();
+
+    }
+
+    protected sql() {
+
+        return this.repository.createQueryBuilder('user');
 
     }
 
