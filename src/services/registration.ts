5cc4bfba6a5ff703dd3a2f21ab3553a8bead515f
hitachi567 <abidai790@gmail.com>
2022-01-09 14:53:07 +0300

    added registration service

---
+++ src/services/registration.ts
@@ -0,0 +1,52 @@
+import { Middleware, ApiError } from '@hitachi567/core';
+import { body } from 'express-validator';
+import ConfirmationService from './confirmation';
+import User from '../entities/user';
+
+export default class RegistrationService extends ConfirmationService {
+
+    static checkBodyEmailValid(): Middleware {
+
+        let middleware = body('email');
+        middleware.notEmpty().withMessage('email were expected, but not received');
+        middleware.isString().withMessage('email must be string');
+        middleware.isEmail().withMessage('email must be valid');
+        middleware.trim();
+
+        return middleware;
+
+    }
+
+    async checkEmailUniqueness(email: string): Promise<void> {
+
+        let user = await this.userRepository.findOneByEmail(email);
+
+        if (user) {
+            throw ApiError.BadRequest('email occupied');
+        }
+
+    }
+
+    async createUser(email: string): Promise<User> {
+
+        let user = User.init(email);
+
+        return this.userRepository.save(user);
+
+    }
+
+    async changeEmail(user: User, newEmail: string): Promise<User> {
+
+        if (user.isConfirmed) {
+
+            throw ApiError.Forbidden('old email already confirmed');
+
+        }
+
+        user.email = newEmail;
+
+        return this.userRepository.save(user);
+
+    }
+
+}
