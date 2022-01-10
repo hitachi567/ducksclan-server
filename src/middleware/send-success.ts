5e8598a94e281a847c41650e7d86162aff1fc990
hitachi567 <abidai790@gmail.com>
2022-01-10 20:23:28 +0300

    completed email registration's routs

---
+++ src/middleware/send-success.ts
@@ -0,0 +1,16 @@
+import { Middleware } from '@hitachi567/core';
+import { ResponseBody } from '../interfaces';
+
+export default function sendSucces(): Middleware {
+    return (request, response) => {
+
+        let body: ResponseBody<undefined> = {
+            status: 200,
+            message: 'success',
+            payload: undefined
+        }
+
+        response.status(200).json(body);
+
+    }
+}
--- src/routers/registration.ts
+++ src/routers/registration.ts
@@ -4,17 +4,22 @@ import authenticate from '../middleware/authenticate';
 import registerEmail from '../auth/registration/register-email';
 import changeEmail from '../auth/registration/change-email';
 import confirmEmail from '../auth/registration/confirm-email';
+import tokenIssuance from '../auth/token-issuance';
+import sendSucces from '../middleware/send-success';
 
 export const EmailRegistrationRouter = Router()
     .post('/register',
         BodyValidationService.EmailBody(),
-        registerEmail()
+        registerEmail(),
+        tokenIssuance()
     )
     .post('/change',
         BodyValidationService.EmailBody(),
         authenticate(),
-        changeEmail()
+        changeEmail(),
+        tokenIssuance()
     )
     .get('/confirm/:link',
-        confirmEmail()
+        confirmEmail(),
+        sendSucces()
     );
