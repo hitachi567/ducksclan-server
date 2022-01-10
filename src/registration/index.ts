c2fabb8a4f6bf6f640b0c4b072c65c05ee8e1f7d
hitachi567 <abidai790@gmail.com>
2022-01-10 07:04:29 +0300

    added email registration router

--- src/registration/index.ts
+++ src/registration/index.ts
@@ -1,3 +1,23 @@
 import { Timeout } from '@hitachi567/core';
+import { Router } from 'express';
+import BodyValidationService from '../services/body.validation';
+import authenticate from '../middleware/authenticate';
+import registerEmail from './register-email';
+import changeEmail from './change-email';
+import confirmEmail from './confirm-email';
 
 export const rejectRegistrationTimout = new Timeout();
+
+export const EmailRegistrationRouter = Router()
+    .post('/register',
+        BodyValidationService.EmailBody(),
+        registerEmail()
+    )
+    .post('/change',
+        BodyValidationService.EmailBody(),
+        authenticate(),
+        changeEmail()
+    )
+    .get('/confirm/:link',
+        confirmEmail()
+    );
