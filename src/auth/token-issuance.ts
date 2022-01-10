b91e8dde1f297166d1685147e37375a6d226bd92
hitachi567 <abidai790@gmail.com>
2022-01-10 17:47:28 +0300

    added token issuance middleware

---
+++ src/auth/token-issuance.ts
@@ -0,0 +1,41 @@
+import { TokensPair, Middleware, asyncMiddleware } from '@hitachi567/core';
+import { LocalsWithUser, ResponseBody } from '../interfaces';
+import { Transaction } from '../database';
+import Database from '../database/index';
+import TokenIssuanceService from '../services/token-issuance';
+
+function transaction(locals: LocalsWithUser): Transaction<TokensPair> {
+    return async manager => {
+
+        TokenIssuanceService.checkLocals(locals);
+
+        const service = new TokenIssuanceService(locals, manager);
+
+        let pair = service.generatePair();
+
+        await service.saveRefresh(pair.refresh);
+
+        return pair;
+
+    }
+}
+
+export default function tokenIssuance(): Middleware<any, LocalsWithUser> {
+    return asyncMiddleware(async (request, response, next) => {
+
+        let pair = await Database.transaction<TokensPair>(
+            transaction(response.locals)
+        );
+
+        let body: ResponseBody = {
+            status: 200,
+            message: 'success',
+            payload: {
+                token: pair.access
+            }
+        }
+
+        response.status(body.status).json(body);
+
+    });
+}
