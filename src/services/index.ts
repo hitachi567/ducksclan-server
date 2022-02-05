78f16e81ea795b876c07bcaf4d8a06f9620c797f
hitachi567 <abidai790@gmail.com>
2022-02-05 23:08:54 +0300

    added common entry point for services

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+
+export * from './token.service';
+export * from './user.confirmation.service';
+export * from './user.creation.service';
+export * from './user.credential.service';
+export * from './user.finding.service';
+export * from './user.service';
