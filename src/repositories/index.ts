a5821fa8c4c21d277bf94bd21d9a1fa72eff7b10
hitachi567 <abidai790@gmail.com>
2022-02-05 20:43:45 +0300

    modified getting custom repositories

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,21 +1,28 @@
 import { EntityManager } from 'typeorm';
 import Database from '../database';
 import UserRepository from './user.repository';
-import RefreshTokenRepository from './refresh-token.repository';
+import TokenRefreshRepository from './token.refresh.repository';
 import ConfirmLinkRepository from './confirm.link.repository';
 
-export default abstract class Repositories {
+interface IRepositories {
+    user: UserRepository;
+    tokenRefresh: TokenRefreshRepository;
+    confirmLink: ConfirmLinkRepository;
+}
 
-    protected userRepository: UserRepository;
-    protected refreshTokenRepository: RefreshTokenRepository;
-    protected confirmLinkRepository: ConfirmLinkRepository;
+export default abstract class Repositories {
     protected manager: EntityManager;
+    protected reps: IRepositories;
 
     constructor(manager?: EntityManager) {
-        this.manager = manager || Database.manager;
-        this.userRepository = this.manager.getCustomRepository(UserRepository);
-        this.refreshTokenRepository = this.manager.getCustomRepository(RefreshTokenRepository);
-        this.confirmLinkRepository = this.manager.getCustomRepository(ConfirmLinkRepository);
-    }
+        this.manager = manager || Database.instance.manager;
 
+        const getRep = this.manager.getCustomRepository;
+
+        this.reps = {
+            user: getRep(UserRepository),
+            tokenRefresh: getRep(TokenRefreshRepository),
+            confirmLink: getRep(ConfirmLinkRepository),
+        }
+    }
 }
