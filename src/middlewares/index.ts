635880b8fd1f1a3e4eb1ae19b264d4a740ea861d
hitachi567 <abidai790@gmail.com>
2022-02-05 21:35:28 +0300

    removed export definition of unusage files

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,5 +3,3 @@ export * from './authenticate';
 export * from './handle-validate-result';
 export * from './send-success';
 export * from './token-issuance';
-export * from './set-user';
-export * from './confirm-email';
