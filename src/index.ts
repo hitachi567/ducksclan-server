80b0c67e59934b75e3c78a0012ff4afcb094c050
hitachi567 <abidai790@gmail.com>
2022-02-05 21:38:12 +0300

    corrected imports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import App from './app';
-import Database from './database/database';
+import Database from './database';
+import entities from './entities';
 
 export const app = new App(App.getConfiguration());
 
 main().catch(console.log);
 
 async function main() {
-    await Database.init([]);
+    await Database.init(entities);
     app.listen();
 }
