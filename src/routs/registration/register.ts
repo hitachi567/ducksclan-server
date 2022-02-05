247bfb0f51ff229c263acfcb41ad4befe84c4da0
hitachi567 <abidai790@gmail.com>
2022-02-05 17:05:13 +0300

    added register rout

---
+++ src/routs/registration/register.ts
@@ -0,0 +1,36 @@
+import { Middleware, asyncMiddleware } from '@hitachi567/core';
+import { EmailBody, LocalsWithUser, Transaction } from '../../interfaces';
+import { UserCreationService, UserConfirmationService } from '../../services';
+import { User } from '../../entities';
+import Database from '../../database/database';
+
+function transaction(body: EmailBody): Transaction<User> {
+    return async manager => {
+
+        const creation = new UserCreationService(manager);
+
+        let result = await creation.create(body.email);
+
+        const confirmation = new UserConfirmationService(result.user, manager);
+
+        confirmation.sendMail(result.confirmLink.payload);
+        confirmation.setTimeout();
+
+        return result.user;
+
+    }
+}
+
+export default function register(): Middleware<EmailBody, LocalsWithUser> {
+    return asyncMiddleware(async (request, response, next) => {
+
+        let user = await Database.instance.transaction(
+            transaction(request.body)
+        );
+
+        response.locals.user = user;
+
+        next();
+
+    });
+}
